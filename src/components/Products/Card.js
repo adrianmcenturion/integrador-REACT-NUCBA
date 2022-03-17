d623c8017a19faba8a6af5632c44c6966685d7fd
adrianmcenturion <adrianmcenturion@gmail.com>
2022-03-17 16:57:32 -0300

    fix card

--- src/components/Products/Card.js
+++ src/components/Products/Card.js
@@ -1,4 +1,4 @@
-import { Badge, Box, Flex, Image, Text, Button, Spacer, useToast } from "@chakra-ui/react"
+import { Badge, Box, Flex, Image, Text, Button, Spacer, useToast, Spinner } from "@chakra-ui/react"
 import { useDispatch, useSelector } from "react-redux"
 import typeColor from "./TypesColors"
 import { AddToCartAction } from "../../redux/CartSlice"
@@ -20,47 +20,44 @@ const Card = () => {
         })
     }
 
-    // const pokeCardsData = useSelector(state => state.products.productsData)
-    // console.log(pokeCardsData)
+    const pokeCardsData = useSelector(state => state.products.productsData)
+    console.log(pokeCardsData)
 
-    // const mappedCards = pokeCardsData.map((card, id) => {
+    const mappedCards = pokeCardsData.map((card, id) => {
 
-    //     return (
+        return (
 
-    //     <Box overflow='hidden' borderWidth='1px' borderRadius='lg' key={card.id}>
-    //         <Image boxSize='fit-content' maxW='150px' src={card.images.small} alt={card.name}></Image>
-    //         <Box p='2' mx='auto' maxW='fit-content' height='fit-content'>
-    //                 <Box display='flex' flexDirection='column' alignItems='flex-start' gap='2'>
-    //                     <Text fontSize='md'>{card.name}</Text>
+        <Box overflow='hidden' borderWidth='1px' borderRadius='lg' key={card.id}>
+            <Image boxSize='fit-content' maxW='150px' src={card.images.small} alt={card.name}></Image>
+            <Box p='2' mx='auto' maxW='fit-content' height='fit-content'>
+                    <Box display='flex' flexDirection='column' alignItems='flex-start' gap='2'>
+                        <Text fontSize='md'>{card.name}</Text>
                         
-    //                     <Flex >
-    //                         {card.types === undefined ? null : <Text textAlign='left' fontSize='x-small'>Type: </Text>}
-    //                         <Box display='flex' flexWrap='wrap' gap={1} alignItems='center'>
-    //                         {card.types === undefined ? null : (card.types).map((type, id) => {return <Badge ml='2' borderRadius='base' px='2' key={id} colorScheme={typeColor[type]} textAlign='center' fontSize='x-small' fontWeight='semibold'>{type}</Badge>})}
-    //                         </Box>
-    //                     </Flex>
-    //                     <Box display='flex' flexDirection='column' justify='space-around' gap='1' alignItems='flex-start'>
-    //                         <Text fontSize='x-small'>Set: {card.set.name}</Text>
-    //                         <Text fontSize='x-small'>Rarity: {card.rarity}</Text>
-    //                         <Flex gap='1' alignItems='flex-start' flexDirection='column'>
-    //                             <Text fontSize='x-small'>Price: ${card.price}</Text>
-    //                         </Flex>
-    //                     </Box>
-    //                     <Spacer/>
-    //                 </Box>
-    //                 <Button mx='auto' color='white' colorScheme='newRed' size='xs' fontSize='xx-small' onClick={() => handleClickAddToCart(card)} >ADD TO CART</Button>
-    //         </Box>
-    //     </Box>
-    //     )
-    // })
+                        <Flex >
+                            {card.types === undefined ? null : <Text textAlign='left' fontSize='x-small'>Type: </Text>}
+                            <Box display='flex' flexWrap='wrap' gap={1} alignItems='center'>
+                            {card.types === undefined ? null : (card.types).map((type, id) => {return <Badge ml='2' borderRadius='base' px='2' key={id} colorScheme={typeColor[type]} textAlign='center' fontSize='x-small' fontWeight='semibold'>{type}</Badge>})}
+                            </Box>
+                        </Flex>
+                        <Box display='flex' flexDirection='column' justify='space-around' gap='1' alignItems='flex-start'>
+                            <Text fontSize='x-small'>Set: {card.set.name}</Text>
+                            <Text fontSize='x-small'>Rarity: {card.rarity}</Text>
+                            <Flex gap='1' alignItems='flex-start' flexDirection='column'>
+                                <Text fontSize='x-small'>Price: ${card.price}</Text>
+                            </Flex>
+                        </Box>
+                        <Spacer/>
+                    </Box>
+                    <Button mx='auto' color='white' colorScheme='newRed' size='xs' fontSize='xx-small' onClick={() => handleClickAddToCart(card)} >ADD TO CART</Button>
+            </Box>
+        </Box>
+        )
+    })
 
 
   return (
 
-        <>
-        {/* {mappedCards} */}
-        <h2>awsd</h2>
-        </>
+        <>{pokeCardsData.length > 0 ?  mappedCards : <Flex gap={4} alignItems='center'><Text>Loading</Text><Spinner size='md' thickness="2px" emptyColor="red.500" color="white.500"/></Flex>}</>
   )
 }
 
