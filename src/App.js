08c42b919e72d3884258fefbc1bde8ac6a004278
adrianmcenturion <adrianmcenturion@gmail.com>
2022-05-25 20:45:26 -0300

    fix title

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@
 import './App.css';
 import Router from './Router/Router';
 import { Provider } from 'react-redux';
-// import generateStore, { Persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import  store, { Persistor } from './redux/store';
 import { GetTotalsAction } from './redux/CartSlice';
 
 function App() {
-
-  // const store = generateStore();
 
   store.dispatch(GetTotalsAction())
 
